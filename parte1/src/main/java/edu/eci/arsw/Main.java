ad1d694f9cf32a65113c210d33b8dd9a5f4e9a25
Andres Jacobo <andres.sepulveda-s@mail.escuelaing.edu.co>
2025-08-27 13:01:34 -0500

    Add: Code for part c in point 3

--- parte1/src/main/java/edu/eci/arsw/Main.java
+++ parte1/src/main/java/edu/eci/arsw/Main.java
@@ -1,29 +1,47 @@
 package main.java.edu.eci.arsw;
 
-
 import main.java.edu.eci.arsw.threads.PrimeFinderThread;
 
 import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Main {
 
 	public static void main(String[] args) {
 		int r = (Integer.MAX_VALUE / 3) * 2;
-		
-		PrimeFinderThread pft=new PrimeFinderThread(0, (200000000/3)-1);
-		PrimeFinderThread ptf2=new PrimeFinderThread(200000000/3,2*(200000000/3));
-		PrimeFinderThread ptf3=new PrimeFinderThread(2*(200000000/3)+1,200000000);
-		
-		pft.start();
-		ptf2.start();
-		ptf3.start();
-		Scanner scanner=new Scanner(System.in);
-		System.out.println("Presiona enter para reanudar ....... " );
-		scanner.nextLine();
-		pft.resumeThread();
-		ptf2.resumeThread();
-		ptf3.resumeThread();
+		PrimeFinderThread pft1=new PrimeFinderThread(0, (200000000/3)-1);
+		PrimeFinderThread pft2=new PrimeFinderThread(200000000/3,2*(200000000/3));
+		PrimeFinderThread pft3=new PrimeFinderThread(2*(200000000/3)+1,200000000);
+		pft1.start();
+		pft2.start();
+		pft3.start();
 
+		Timer timer = new Timer();
+		Scanner scanner = new Scanner(System.in);
+		timer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				// Interrumpe los hilos
+				pft1.pauseThread();
+				pft2.pauseThread();
+				pft3.pauseThread();
+				int totalPrimes = pft1.getPrimes() + pft2.getPrimes() + pft3.getPrimes();
+				System.out.println("El numero de primos encontrados hasta el momento es de: " + totalPrimes);
+				System.out.println("Presione Enter para continuar.");
+
+				String isEnter = scanner.nextLine();
+				while (!isEnter.equals("")) {
+					System.out.println("Presione Enter para continuar.");
+					isEnter = scanner.nextLine();
+				}
+				pft1.resumeThread();
+				pft2.resumeThread();
+				pft3.resumeThread();
+
+				timer.cancel(); // Detiene el temporizador
+			}
+		}, 5000);
 	}
 }
 	
